aa3d95033d3cbe1654bff0b49e48fef78a0fd2df
HIIMHEY <changabriel163@gmail.com>
2026-08-21 06:39:33 +0800

    Implement A-Enums task types

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,12 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected TaskType type;
 
-    public Task(String description) {
+    public Task(String description, TaskType type) {
         this.description = description;
         this.isDone = false;
+        this.type = type;
     }
 
     public void markAsDone() {
@@ -25,6 +27,7 @@ public class Task {
 
     @Override
     public String toString() {
-        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+        return "[" + this.type.getSymbol() + "][" + this.getStatusIcon() + "] "
+                + this.getDescription();
     }
 }
